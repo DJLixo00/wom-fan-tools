73a8e859d08cadcd0074cb7fc55590c3166ff30a
djlixo00 <heusethisforgames@gmail.com>
2022-06-08 15:27:23 -0700

    added MainBox component for the 4 main divs

---
+++ src/components/MainBox.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+function MainBox() {
+  return (
+    <div className="mainBox">
+      <div id="topDiv" className="mainDiv">top</div>
+      <div id="leftDiv" className="mainDiv">left</div>
+      <div id="rightDiv" className="mainDiv">right</div>
+      <div id="bottomDiv" className="mainDiv">bottom</div>
+    </div>
+  );
+}
+
+export default MainBox;
